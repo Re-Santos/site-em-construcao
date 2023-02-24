5a2864b37b78c8ad01949b3157215794aa01efa3
Re-Santos <renata.prog@gmail.com>
2023-02-23 21:24:31 -0300

    Alterações css, imagem e texto

--- script.js
+++
@@ -1,7 +0,0 @@
-const img = document.createElement('./img/hero-desktop.jpg');
-
-img.src = 'http://www.google.com/intl/en_ALL/images/logo.gif';
-var src = document.createElement('source');
-src.innerHTML = 'Hello World';
-document.body.appendChild(img);
-document.body.appendChild(src);
